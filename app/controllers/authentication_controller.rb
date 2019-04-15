c47c9587f8d6703258ccaa8632ba52313c41d9c8
Cristian Leiva <cleiva94.cl@gmail.com>
2019-04-15 15:07:42 -0400

    auth done

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,25 @@
+class AuthenticationController < ApplicationController
+  def create
+    @user = User.find_by(username: user_login_params[:name])
+    if @user && @user.authenticate(user_login_params[:password])
+      token = JWT.encode({ user_id: @user.id }, "secret")
+      render json: { user:@user , jwt: token }, status: :accepted
+    else
+      render json: { message: 'Invalid username or password' }, status: :unauthorized
+    end
+  end
+
+  def show
+    user_id = JWT.decode(request.headers["Authorization"], "secret", false)[0]["user_id"]
+    user = User.find(user_id)
+    render json: { user: user}
+    # This is the token: request.headers["Authorization"]
+  end
+
+
+  private
+
+  def user_login_params
+    params.require(:user).permit(:username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pokemons
+
+  resources :users
+  post '/login', to: 'authentication#create'
+  get '/current_user', to: 'authentication#show'
 end
