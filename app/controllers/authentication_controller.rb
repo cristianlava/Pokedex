d69ad8a916bc5c9d736efc3ff1b8db024158fb8f
Cristian Leiva <cleiva94.cl@gmail.com>
2019-04-15 17:36:07 -0400

    backend

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,7 +3,7 @@ class AuthenticationController < ApplicationController
     @user = User.find_by(email: user_login_params[:email])
     if @user && @user.authenticate(user_login_params[:password])
       token = JWT.encode({ user_id: @user.id }, "secret")
-      render json: { user:@user , jwt: token }, status: :accepted
+      render json: { user:@user, pokemons:@user.user_pokemons , jwt: token }, status: :accepted
     else
       render json: { message: 'Invalid username or password' }, status: :unauthorized
     end
@@ -13,7 +13,6 @@ class AuthenticationController < ApplicationController
     user_id = JWT.decode(request.headers["Authorization"], "secret", false)[0]["user_id"]
     user = User.find(user_id)
     render json: { user: user}
-    # This is the token: request.headers["Authorization"]
   end
 
 
---
+++ app/serializers/user_pokemon_serializer.rb
@@ -0,0 +1,5 @@
+class UsersPokemonsSerializer < ActiveModel::Serializer
+  attributes :id, :user_id, :pokemon_id
+  belongs_to :user
+  belongs_to :pokemon
+end
